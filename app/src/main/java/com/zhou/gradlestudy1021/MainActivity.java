e04e519486b9c2e343cdf82498933adff2b07f40
9208 <9208@dev198.com>
2019-10-21 16:15:02 +0800

    提交多渠道打包的关键代码

--- app/src/main/java/com/zhou/gradlestudy1021/MainActivity.java
+++ app/src/main/java/com/zhou/gradlestudy1021/MainActivity.java
@@ -1,8 +1,11 @@
 package com.zhou.gradlestudy1021;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvCurTheme = findViewById(R.id.curTheme);
+        TextView tvCurChannel = findViewById(R.id.curChannel);
+
+        getMetaData();
+
+        tvCurTheme.setText(theme);
+        tvCurChannel.setText(channel);
+
+    }
+
+    String channel;
+    String theme;
+
+
+    /**
+     * 从manifest中读取出metadata
+     */
+    private void getMetaData() {
+        PackageManager pm = getPackageManager();
+        try {
+            ApplicationInfo appInfo = pm.getApplicationInfo(BuildConfig.APPLICATION_ID, PackageManager.GET_META_DATA);
+            Bundle bd = appInfo.metaData;
+            channel = bd.getString("CHANNEL");
+            theme = bd.getString("THEME");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
